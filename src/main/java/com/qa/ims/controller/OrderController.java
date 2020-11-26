136fe4c0a9e588a2ee4c4e27993ca02140f76379
Jason-Fyfe <JFyfe@academytrainee.com>
2020-11-26 10:03:28 +0000

    JF: IMSP-18,19,21 completed. Massive overhaul of most project structure to accommodate for Creating, Reading, Updating and Deleting Orders.

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.qa.ims.controller;
 
+import com.qa.ims.persistence.dao.ItemDAO;
 import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderController implements CrudController<Order>
@@ -13,16 +16,18 @@ public class OrderController implements CrudController<Order>
     public static final Logger LOGGER = LogManager.getLogger();
 
     private OrderDAO orderDAO;
+    private ItemDAO itemDAO;
     private Utils utils;
 
-    public OrderController(OrderDAO orderDAO, Utils utils) {
+    public OrderController(OrderDAO orderDAO, ItemDAO itemDAO, Utils utils) {
         super();
         this.orderDAO = orderDAO;
+        this.itemDAO = itemDAO;
         this.utils = utils;
     }
 
     /**
-     * Reads all customers to the logger
+     * Reads all orders to the logger
      */
     @Override
     public List<Order> readAll() {
@@ -34,33 +39,81 @@ public class OrderController implements CrudController<Order>
     }
 
     /**
-     * Creates a customer by taking in user input
+     * Creates an order by taking in user input
      */
     @Override
-    public Order create() {
+    public Order create()
+    {
+        List<Item> orderItems = new ArrayList<>();
         LOGGER.info("Please enter the customer ID for the order");
         Long customerID = utils.getLong();
-        Order order = orderDAO.create(new Order(customerID));
+
+        boolean addNewItem;
+        do
+        {
+            LOGGER.info("Please enter the item ID to add to the order");
+            Long itemID = utils.getLong();
+
+            orderItems.add(itemDAO.readItem(itemID));
+
+            LOGGER.info("Add another item\nYes | No");
+            String choice = utils.getString();
+
+            addNewItem = choice.toLowerCase().equals("yes");
+        }
+        while (addNewItem);
+
+        Order order = orderDAO.create(new Order(customerID, orderItems));
+
         LOGGER.info("Order created");
         return order;
     }
 
     /**
-     * Updates an existing customer by taking in user input
+     * Updates an existing order by taking in user input
      */
     @Override
     public Order update() {
+
         LOGGER.info("Please enter the id of the order you would like to update");
         Long id = utils.getLong();
-        LOGGER.info("Please enter the new customer ID");
-        Long customerID = utils.getLong();
-        Order order = orderDAO.update(new Order(id, customerID));
+
+        Order order = orderDAO.readOrder(id);
+
+        boolean editItem = true;
+        do
+        {
+            LOGGER.info("Add: Add an Item to the order");
+            LOGGER.info("Remove: Remove an Item from the order");
+            LOGGER.info("Return: Return to previous menu");
+            String choice = utils.getString().toLowerCase();
+
+            switch (choice)
+            {
+                case "add":
+                    LOGGER.info("The Item ID to add to the order");
+                    long addID = utils.getLong();
+                    Item item = itemDAO.readItem(addID);
+                    order.addItem(item);
+                    break;
+                case "remove":
+                    LOGGER.info("The Item ID to remove from the order");
+                    long removeID = utils.getLong();
+                    order.removeItem(itemDAO.readItem(removeID));
+                    break;
+                case "return":
+                    editItem = false;
+                    break;
+            }
+        }
+        while (editItem);
+
         LOGGER.info("Order Updated");
-        return order;
+        return orderDAO.update(order);
     }
 
     /**
-     * Deletes an existing customer by the id of the customer
+     * Deletes an existing order by the id of the order
      */
     @Override
     public int delete() {
--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,92 +1,144 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class Order
 {
     private Long id;
     private Long customerID;
-    private HashMap<Long, Integer> orderItems;
+    private List<Item> orderItems;
+    private double orderTotal;
 
-    public Order(Long customerID, HashMap<Long, Integer> orderItems) {
+    public Order(Long customerID, List<Item> orderItems)
+    {
         this.customerID = customerID;
         this.orderItems = orderItems;
+        updateOrderTotal();
     }
 
-    // todo remove once hashmap is implemented
-    public Order(Long id, Long customerID) {
+    public Order(Long id, Long customerID, List<Item> orderItems)
+    {
         this.id = id;
         this.customerID = customerID;
+        this.orderItems = orderItems;
+        updateOrderTotal();
     }
-    // todo remove once hashmap is implemented
-    public Order(Long customerID)
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Long getCustomerID()
+    {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID)
     {
         this.customerID = customerID;
     }
 
-    public Order(Long id, Long customerID, HashMap<Long, Integer> orderItems) {
-        this.id = id;
-        this.customerID = customerID;
-        this.orderItems = orderItems;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getCustomerID() {
-        return customerID;
-    }
-
-    public void setCustomerID(Long customerID) {
-        this.customerID = customerID;
-    }
-
-    public HashMap<Long, Integer> getOrderItems() {
+    public List<Item> getOrderItems()
+    {
         return orderItems;
     }
 
-    public void setOrderItems(HashMap<Long, Integer> orderItems) {
+    public void setOrderItems(List<Item> orderItems)
+    {
         this.orderItems = orderItems;
     }
 
-    // todo Iterate through and print all item names + prices, and print customer details.
-    @Override
-    public String toString() {
-        // todo fix toString override once hashmap is implemented
-//        return "id:" + id + " Customer ID:" + customerID + " Items:" + orderItems.size();
-        return "id:" + id + " Customer ID:" + customerID;
+    public double getOrderTotal()
+    {
+        return orderTotal;
+    }
+
+    private void updateOrderTotal()
+    {
+        for (Item i : orderItems)
+        {
+            orderTotal += i.getPrice();
+        }
+    }
+
+    public int getItemQuantity(Item item)
+    {
+        return Collections.frequency(orderItems, item);
+    }
+
+    public HashMap<Integer, Item> getUniqueItems()
+    {
+        HashMap<Integer, Item> uniqueOrderItems = new HashMap<>();
+
+        for (Item i : orderItems)
+        {
+            uniqueOrderItems.put(i.getId().intValue(), i);
+        }
+
+        return uniqueOrderItems;
+    }
+
+    public void addItem(Item item)
+    {
+        orderItems.add(item);
+    }
+
+    public void removeItem(Item item)
+    {
+        orderItems.remove(item);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order ID: ").append(id).append(" | Customer ID: ").append(customerID).append(" | Order Total: ").append(orderTotal);
+        HashMap<Integer, Item> uniqueItems = getUniqueItems();
+        if (orderItems != null)
+        {
+            uniqueItems.forEach((k,item) -> {
+                sb.append("\n  --").append(item.toString()).append(" | Quantity: ").append(getItemQuantity(item));
+            });
+        }
+        else
+        {
+            sb.append("\n This order has no items!");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) { return true; }
+        if (obj == null) { return false; }
+        if (getClass() != obj.getClass()) { return false; }
         Order other = (Order) obj;
-        if (customerID == null) {
-            if (other.customerID != null)
-                return false;
-        } else if (!customerID.equals(other.customerID))
-            return false;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (orderItems == null) {
-            if (other.orderItems != null)
-                return false;
-        } else if (!orderItems.equals(other.orderItems))
-            return false;
+
+        if (customerID == null)
+        {
+            if (other.customerID != null) { return false; }
+        }
+        else if (!customerID.equals(other.customerID)) { return false; }
+
+        if (id == null)
+        {
+            if (other.id != null) { return false; }
+        }
+        else if (!id.equals(other.id)) { return false; }
+        if (orderItems == null)
+        {
+            if (other.orderItems != null) { return false; }
+        }
+        else if (!orderItems.equals(other.orderItems)) { return false; }
         return true;
     }
-
 }
